a04e15b7d9219ec1bb32bb0c9ab2dc25e4ce9bcb
alexn <alex.notore.01@gmail.com>
2023-07-14 15:18:33 +0200

    Inizio grafica e heder

--- index.php
+++ index.php
@@ -4,33 +4,144 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Web Validator</title>
+  <title>ValURL</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
   <script>
-    function createTabella(response) {
-      var risultato = response;
+    function InviaInput() {
+      const urlInput = document.getElementById('formURL');
       var tabella = document.getElementById("tabella-risultati");
 
-      var markup = "<tr><th>Codice</th><th>Cliente</th><th>Data inizio</th>"
-      markup += "<th>Indirizzo</th><th>Città</th><th></th><th></th><th></th><th></th></tr>";
+      const url = urlInput.value;
 
-      for (var i = 0; i < risultato.length; i++) {
+      if (url == "") {
+        $('#modalIncorrect').modal('show');
+        tabella.innerHTML = "";
+
+      } else {
+        extractLinks();
+      }
+    }
+
+    function extractLinks() {
+      const urlInput = document.getElementById('formURL');
+      var tabella = document.getElementById("tabella-risultati");
+      const url = urlInput.value;
+
+      // Fetch the HTML content of the web page
+      fetch(url)
+        .then(response => response.text())
+        .then(html => {
+          // Parse the HTML using DOMParser
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(html, 'text/html');
+
+          // Extract all the <a> elements and their href attributes
+          const links = doc.querySelectorAll('a');
+          const linkUrls = Array.from(links).map((link) => {
+            let href = link.href;
+            // Remove the initial portion matching "http://127.0.0.1:5500" and replace it with urlInput
+            if (href.startsWith("http://localhost")) {
+              href =
+                urlInput.value +
+                href.substring("http://localhost".length);
+            }
+            return href;
+          });
+
+
+          var markup = "<tr><th>Link Disponibili</th></tr>";
+
+          for (var i = 0; i < linkUrls.length; i++) {
+
+            markup += "<tr>";
+
+            /*
+            Gestire condizione per il colore
+
+            <span class="badge badge-pill badge-success"></span>
+            <span class="badge badge-pill badge-danger"></span>
+            <span class="badge badge-pill badge-warning"></span>
+            */
+
+            markup += "<td class='align-middle'> <button type='button' name='pulsanteEsaminaLink' class='btn' data-bs-toggle='modal' id='" + linkUrls[i] + "'>" + linkUrls[i] + "</button> </td>";
+            markup += "</tr>";
+          }
+
+          tabella.innerHTML = markup;
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    }
+
+    function createTabella() {
+      var tabella = document.getElementById("tabella-risultati");
+      //var markup = "<tr><th></th><th>Link</th><th>Sicurezza</th><th>Supporto HTTPS</th></tr>";
+
+      var markup = "<tr><th>Link Disponibili</th></tr>";
+
+      for (var i = 0; i < linkUrls.length; i++) {
         markup += "<tr>";
-        markup += "<td class='align-middle'>" + risultato[i].codCantiere + "</td><td class='align-middle'>" + risultato[i].cliente + "</td><td class='align-middle'>" + risultato[i].dataInizio + "</td>";
-        if (checkSwitch) {
-          markup += "<td class='align-middle'>" + risultato[i].dataFine + "</td>";
-        }
-        markup += "<td class='align-middle'>" + risultato[i].indirizzo + "</td><td class='align-middle'>" + risultato[i].citta + "</td>";
-        markup += "<td class='align-middle'> <button type='button' class='btn btn-warning' data-bs-toggle='modal' data-bs-target='#modaleInterventi' name='pulsanteInterventi' id='" + risultato[i].codCantiere + "' >Gestisci interventi</button> </td>";
-        markup += "<td class='align-middle'> <button type='button' class='btn btn-warning' data-bs-toggle='modal' data-bs-target='#modaleMateriali' name='pulsanteMateriali' id='" + risultato[i].codCantiere + "' >Gestisci materiali</button> </td>";
-        markup += "<td class='align-middle'> <button type='button' class='btn btn-warning' data-bs-toggle='modal' data-bs-target='#modaleOperai' name='pulsanteOperai' id='" + risultato[i].codCantiere + "' >Gestisci operai</button> </td>";
-        markup += "<td class='align-middle'> <button type='button' class='btn btn-danger' data-bs-toggle='modal' data-bs-target='#modaleElimina' name='pulsanteElimina' id='" + risultato[i].codCantiere + "' >Elimina</button> </td>";
+
+        /*
+        Gestire condizione per il colore
+
+        <span class="badge badge-pill badge-success"></span>
+        <span class="badge badge-pill badge-danger"></span>
+        <span class="badge badge-pill badge-warning"></span>
+        */
+
+        markup += "<td class='align-middle'> <button type='button' name='pulsanteEsaminaLink' class='btn' data-bs-toggle='modal' id='" + linkUrls[i] + "'>" + linkUrls[i] + "</button> </td>";
         markup += "</tr>";
       }
 
       tabella.innerHTML = markup;
     }
+
+    $(document).on('click', 'button[name="sendButton"]', function() {
+      InviaInput();
+    });
+
+    document.addEventListener("keydown", function(event) {
+      if (event.keyCode === 13) {
+        event.preventDefault(); // Previene l'invio del modulo se presente
+        InviaInput()
+      }
+    });
+
+    $(document).on('click', 'button[name="pulsanteEsaminaLink"]', function() {
+      var url = $(this).attr('id');
+      var modaleInfoURLHeader = document.getElementById("modaleInfoURLHeader");
+      var headersID = document.getElementById("tabella-headersLink");
+      modaleInfoURLHeader.innerHTML = url;
+
+      $.ajax({
+        url: url,
+        method: 'HEAD',
+        success: function(data, textStatus, jqXHR) {
+          $('#modaleInfoURL').modal('show');
+          const headers = jqXHR.getAllResponseHeaders();
+          const headersList = headers.split("\n");
+
+          var markup = "<tr><th>Headers</th></tr>";
+
+          for (var i = 0; i < headersList.length; i++) {
+            markup += "<tr>";
+            markup += "<td class='align-middle'>" + headersList[i] + "</td>";
+            markup += "</tr>";
+          }
+
+          headersID.innerHTML = markup;
+        },
+
+        error: function(data, textStatus, jqXHR) {
+          $('#modalCORSPolicy').modal('show');
+        }
+      });
+
+    });
   </script>
 </head>
 
@@ -45,7 +156,7 @@
         <div class="col">
           <div class="card border-0">
             <div class="card-body">
-              <h2 class="card-title">Web Validator</h2>
+              <h2 class="card-title">ValURL</h2>
             </div>
           </div>
         </div>
@@ -55,7 +166,7 @@
         <div class="col-md-11">
           <div class="card border-0">
             <div class="card-body">
-              <input type="url" autocomplete="off" class="form-control" id="formURL" name="formURL">
+              <input type="url" autocomplete="off" class="form-control" id="formURL" name="formURL" placeholder="URL">
             </div>
           </div>
         </div>
@@ -83,81 +194,68 @@
             </div>
           </div>
         </div>
+      </div>
 
-        <!-- Controls -->
+
+      <!-- Controls -->
 
 
-        <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" id="modaleAggiungi">
-          <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h2 class="modal-title h5 " id="modaleAggiungi">Nuovo cantiere</h2>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Chiudi finestra modale"></button>
+      <div class="modal fade bd-example-modal-xl" tabindex="-1" role="dialog" id="modaleInfoURL">
+        <div class="modal-dialog modal-xl">
+          <div class="modal-content">
+
+            <div class="modal-header">
+              <label id="modaleInfoURLHeader"></label>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Chiudi finestra modale"></button>
+            </div>
+
+            <div class="modal-body">
+              <div id="tabella">
+                <table class='table table-hover' id="tabella-headersLink">
+                </table>
               </div>
-
-              <div class="modal-header">
-                <h2 class="modal-title h5 " id="modaleAggiungi">Nuovo cantiere</h2>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Chiudi finestra modale"></button>
-              </div>
-
-              <div class="modal-body">
-                <form id="nuovoCantiere-form" method="POST">
-                  <div class="mb-3">
-                    <h5 class="card-subtitle mb-2 text-muted">Cliente</h5>
-                    <input type="text" autocomplete="off" class="form-control" id="nomeClienteAdd" name="nomeClienteAdd" placeholder="">
-                  </div>
-
-                  <div class="mb-3">
-                    <h5 class="card-subtitle mb-2 text-muted">Data inizio</h5>
-                    <input type="date" id="dataInizioAdd" name="dataInizioAdd">
-                  </div>
-
-                  <div class="mb-3">
-                    <h5 class="card-subtitle mb-2 text-muted">Indirizzo</h5>
-                    <input type="text" autocomplete="off" class="form-control" id="indirizzoAdd" name="indirizzoAdd" placeholder="">
-                  </div>
-
-                  <div class="mb-3">
-                    <h5 class="card-subtitle mb-2 text-muted">Citta</h5>
-                    <input type="text" autocomplete="off" class="form-control" id="cittaAdd" name="cittaAdd" placeholder="">
-                  </div>
-              </div>
-              <div class="modal-footer">
-                <button class="btn btn-success" type="submit" name="Invia">Aggiungi</button>
-              </div>
-              </form>
-
-              <?php
-              if (isset($_POST['Invia'])) {
-                $_POST['nomeClienteAdd'] = '';
-                $_POST['dataInizioAdd'] = '';
-                $_POST['indirizzoAdd'] = '';
-                $_POST['cittaAdd'] = '';
-              }
-              ?>
-
+            </div>
+            <div class="modal-footer">
             </div>
           </div>
         </div>
-
-        <div class="modal fade popconfirm-modal" tabindex="-1" role="dialog" id="modalIncorrect">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title">Url errato</h5>
-              </div>
-              <div class="modal-body">
-                <p>L'url inserito non è corretto</p>
-              </div>
-              <div class="modal-footer">
-                <button class="btn btn-primary btn-sm" type="button" data-bs-dismiss="modal">Ok</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        
       </div>
     </div>
+
+
+    <div class="modal fade popconfirm-modal" tabindex="-1" role="dialog" id="modalIncorrect">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">URL errato</h5>
+          </div>
+          <div class="modal-body">
+            <p>L'url inserito non è corretto</p>
+          </div>
+          <div class="modal-footer">
+            <button class="btn btn-primary btn-sm" type="button" data-bs-dismiss="modal">Ok</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="modal fade popconfirm-modal" tabindex="-1" role="dialog" id="modalCORSPolicy">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Errore</h5>
+          </div>
+          <div class="modal-body">
+            <p>A causa della politica CORS, non è possibile accedere alle informazioni di questo link.</p>
+          </div>
+          <div class="modal-footer">
+            <button class="btn btn-primary btn-sm" type="button" data-bs-dismiss="modal">Ok</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
   </div>
 </body>
 
